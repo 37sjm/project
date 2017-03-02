698ced59913ad9dc923691bdf12c02a4fddae6f0
Mitch Stark <Mitch Stark>
2017-03-01 20:24:24 -0500

    lab/hw exercises

--- 04modeling/lab04_1.sql
+++ 04modeling/lab04_1.sql
@@ -26,22 +26,68 @@
 --				a person is on multiple teams, we will be able to determine the rest of the data given those three values.
 
 
-drop table AltPerson;
+--Homework 4 - 1.c
+drop table PersonTeam;
+drop table Person;
+drop table Team cascade constraints;
 
-CREATE TABLE AltPerson (
-	personId integer,
+create table Person (
+	id integer primary key,
 	name varchar(10),
 	status char(1),
 	mentorId integer,
-	mentorName varchar(10),
-	mentorStatus char(1),
-    teamName varchar(10),
-    teamRole varchar(10),
-    teamTime varchar(10)
+	foreign key (mentorId) references Person(id) on delete set null
+	);
+	
+create table Team (
+	name varchar(10) primary key,
+	teamTime varchar(10)
+	);
+	
+create table PersonTeam (
+	person_id integer,
+	team_name varchar(10),
+	role varchar(10),
+	primary key (person_id, team_name),
+	foreign key (person_id) references Person(id) on delete cascade,
+	foreign key (team_name) references Team(name) on delete cascade
 	);
 
-INSERT INTO AltPerson VALUES (0, 'Ramez', 'v', 1, 'Shamkant', 'm', 'elders', 'trainee', 'Monday');
-INSERT INTO AltPerson VALUES (1, 'Shamkant', 'm', NULL, NULL, NULL, 'elders', 'chair', 'Monday');
-INSERT INTO AltPerson VALUES (1, 'Shamkant', 'm', NULL, NULL, NULL, 'executive', 'protem', 'Wednesday');
-INSERT INTO AltPerson VALUES (2, 'Jennifer', 'v', 3, 'Jeff', 'm', 'deacons', 'treasurer', 'Tuesday');
-INSERT INTO AltPerson VALUES (3, 'Jeff', 'm', NULL, NULL, NULL, 'deacons', 'chair', 'Tuesday');
+
+-- drop table AltPerson;
+
+-- --CREATE TABLE AltPerson (
+	-- --personId integer,
+	-- name varchar(10),
+	-- status char(1),
+	-- mentorId integer,
+	-- mentorName varchar(10),
+	-- mentorStatus char(1),
+    -- teamName varchar(10),
+    -- teamRole varchar(10),
+    -- teamTime varchar(10)
+	-- );
+
+-- INSERT INTO AltPerson VALUES (0, 'Ramez', 'v', 1, 'Shamkant', 'm', 'elders', 'trainee', 'Monday');
+insert into person values (1, 'Shamkant', 'm', null);
+insert into person values (0, 'Ramez', 'v', 1);
+insert into Team values ('elders', 'Monday');
+insert into personteam values (0, 'elders', 'trainee');
+-- INSERT INTO AltPerson VALUES (1, 'Shamkant', 'm', NULL, NULL, NULL, 'elders', 'chair', 'Monday');
+insert into personteam values (1, 'elders', 'chair');
+-- INSERT INTO AltPerson VALUES (1, 'Shamkant', 'm', NULL, NULL, NULL, 'executive', 'protem', 'Wednesday');
+insert into team values ('executive', 'Wednesday');
+insert into personteam values (1, 'executive', 'protem');
+-- INSERT INTO AltPerson VALUES (2, 'Jennifer', 'v', 3, 'Jeff', 'm', 'deacons', 'treasurer', 'Tuesday');
+insert into person values (3, 'Jeff', 'm', null);
+insert into person values (2, 'Jennifer', 'v', 3);
+insert into team values ('deacons', 'Tuesday');
+insert into personteam values (2, 'deacons', 'treasurer');
+-- INSERT INTO AltPerson VALUES (3, 'Jeff', 'm', NULL, NULL, NULL, 'deacons', 'chair', 'Tuesday');
+insert into personteam values (3, 'deacons', 'chair');
+
+
+--Queries:
+select * from person;
+select * from team;
+select * from personteam;
