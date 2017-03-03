c6230bcf1f5a1db59ccd083ee4f73f2c988dbdfa
mjs73 <mitch.stark.github@gmail.com>
2017-03-03 11:12:48 -0500

    forgot to add user to db in create.sql

--- project/create.sql
+++ project/create.sql
@@ -1,1 +1,22 @@
+drop user mjs73 cascade;
+
+create user mjs73
+identified by password
+quota 5M on System;
+grant
+  connect,
+  create table,
+  create session,
+  create sequence,
+  create view,
+  create materialized view
+  create procedure,
+  create trigger,
+  unlimited tablespace
+  to mjs73;
+
+connect mjs73/password;
+
+--I've simply put all the files in the same directory as sqlplus
 @load
+
